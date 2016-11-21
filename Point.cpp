c6c6097b0b803295d4cd8e7182d8ead7a0419513
othaller <othaller@gmail.com>
2016-11-21 14:43:30 +0200

    Point

---
+++ Point.cpp
@@ -0,0 +1,40 @@
+//
+// Created by Oded Thaller & Roey Shefi
+//
+
+#include <iostream>
+#include "Point.h"
+using namespace std;
+
+    int Point::getX() {
+        return x;
+    }
+
+    int Point::getY() {
+        return y;
+    }
+
+    void Point::setX(int a) {
+        x = a;
+    }
+
+    void Point::setY(int b) {
+        y = b;
+    }
+
+    void Point::print() {
+        cout << "(" << x << "," << y << ")" << "\n";
+    }
+
+    Point::Point(int a, int b) {
+        x = a;
+        y = b;
+    }
+
+    Point::Point() {
+        x = 0;
+        y = 0;
+    }
+
+
+
---
+++ Point.h
@@ -0,0 +1,24 @@
+//
+// Created by Oded Thaller & Roey Shefi
+//
+
+#ifndef PART_2_POINT_H
+#define PART_2_POINT_H
+
+class Point{
+
+    private:
+        int x;
+        int y;
+
+    public:
+        int getX();
+        int getY();
+        void setX(int a);
+        void setY(int b);
+        void print();
+        Point(int a, int b);
+        Point();
+};
+
+#endif //PART_2_POINT_H
